32f931e54defb62ee07bb11c1129e96755a02648
mayurnayak1705 <86305860+mayurnayak1705@users.noreply.github.com>
2021-06-26 18:59:05 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and ReactJS</h1>
+      <p>This was an amazing Bootcamp for frontend web development conducted by ShapeAI from scratch including Javascript,ReactJS And HTML too. </p>
+    </div>
+  );
+}
+export default Info;
